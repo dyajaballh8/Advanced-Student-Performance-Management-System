ec9056c7175c420180bc3de32f2eb881d5d0c8ba
Yara Emad <emadyara39@gmail.com>
2025-12-18 11:34:01 +0200

    Update AttendanceDAO.java

--- src/AttendanceDAO.java
+++ src/AttendanceDAO.java
@@ -1,1 +1,25 @@
+package ada.projectfinal;
 
+import java.sql.*;
+
+public class AttendanceDAO {
+
+    public int getAttendedDays(int studentId) {
+
+        String sql = "SELECT AttendedDays FROM Attendance WHERE StudentID=?";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, studentId);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next())
+                return rs.getInt("AttendedDays");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
