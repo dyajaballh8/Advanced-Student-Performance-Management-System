ed545b61288d6cb1532c4b24664f9f880abf7dfe
Yara Emad <emadyara39@gmail.com>
2025-12-18 11:33:20 +0200

    Add getAllStudents method in StudentDAO
    Implemented method to retrieve all students from the database.

--- src/StudentDAO.java
+++ src/StudentDAO.java
@@ -1,1 +1,39 @@
+package ada.projectfinal;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDAO {
+
+    public List<Student> getAllStudents() {
+
+        List<Student> students = new ArrayList<>();
+
+        String sql =
+            "SELECT s.StudentID, s.Name, u.Password, s.Major, s.GPA, s.TotalHours, s.FacultyName " +
+            "FROM Students s JOIN Users u ON s.StudentID = u.UserID";
+
+        try (Connection con = DBConnection.getConnection();
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Student s = new Student(
+                        rs.getInt("StudentID"),
+                        rs.getString("Name"),
+                        rs.getString("Password"),
+                        rs.getString("Major"),
+                        rs.getDouble("GPA"),
+                        rs.getInt("TotalHours"),
+                        rs.getString("FacultyName")
+                );
+                students.add(s);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+}
