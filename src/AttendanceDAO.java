081e7dd3256a700b3d29430447db740f633a58a4
Yara Emad <emadyara39@gmail.com>
2025-12-18 12:21:21 +0200

    Update AttendanceDAO.java

--- src/AttendanceDAO.java
+++ src/AttendanceDAO.java
@@ -4,6 +4,7 @@ import java.sql.*;
 
 public class AttendanceDAO {
 
+    // جلب أيام الحضور
     public int getAttendedDays(int studentId) {
 
         String sql = "SELECT AttendedDays FROM Attendance WHERE StudentID=?";
@@ -16,6 +17,33 @@ public class AttendanceDAO {
 
             if (rs.next())
                 return rs.getInt("AttendedDays");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    // جلب نسبة الغياب
+    public double getAbsencePercentage(int studentId) {
+
+        String sql = "SELECT TotalDays, AttendedDays FROM Attendance WHERE StudentID=?";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, studentId);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                int totalDays = rs.getInt("TotalDays");
+                int attended = rs.getInt("AttendedDays");
+
+                if (totalDays == 0) return 0;
+
+                int absentDays = totalDays - attended;
+                return (absentDays * 100.0) / totalDays;
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
